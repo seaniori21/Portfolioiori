e3ac8decd9de2d3be2cfddf26812e52e19fa6bc6
Seanevergreen21 <sean@evergreeninterns.club>
2024-11-19 16:34:02 -0500

    updated hotline

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,14 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Portfolio of Sean Umali - Software Developer",
   description:
-    "This is the portfolio of Sean Umali. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+    'This is the portfolio of Sean Umali. '+
+    'I am a highly motivated recent graduate with hands-on experience in software engineering, web development, and data-driven technologies.'+
+          'I thrive in creating innovative, user-centric applications, with a strong passion for both front-end and back-end development.'+ 
+          'My core expertise lies in React, JavaScript, and Python, along with experience in d3.js, SQL, and REST APIs.'+ 
+          'I enjoy exploring new technologies and continuously improving my skills to deliver high-quality, scalable solutions.'+ 
+          'I am passionate about problem-solving and always eager to collaborate in an Agile environment.'+ 
+          'I\'m looking for opportunities to apply my skills in real-world projects and make a meaningful impact.'
+          ,
 };
 
 export default function RootLayout({ children }) {

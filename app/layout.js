7ef321826ff619a444bf245a936fd85546652069
Sean Umali <74208155+seaniori21@users.noreply.github.com>
2024-12-09 15:40:16 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -13,11 +13,11 @@ export const metadata = {
   title: "Portfolio of Sean Umali - Software Developer",
   description:
     'This is the portfolio of Sean Umali. '+
-    'I am a highly motivated recent graduate with hands-on experience in software engineering, web development, and data-driven technologies.'+
-          'I thrive in creating innovative, user-centric applications, with a strong passion for both front-end and back-end development.'+ 
-          'My core expertise lies in React, JavaScript, and Python, along with experience in d3.js, SQL, and REST APIs.'+ 
-          'I enjoy exploring new technologies and continuously improving my skills to deliver high-quality, scalable solutions.'+ 
-          'I am passionate about problem-solving and always eager to collaborate in an Agile environment.'+ 
+    'I am a highly motivated recent graduate with hands-on experience in software engineering, web development, and data-driven technologies. '+
+          'I thrive in creating innovative, user-centric applications, with a strong passion for both front-end and back-end development. '+ 
+          'My core expertise lies in React, JavaScript, and Python, along with experience in d3.js, SQL, and REST APIs. '+ 
+          'I enjoy exploring new technologies and continuously improving my skills to deliver high-quality, scalable solutions. '+ 
+          'I am passionate about problem-solving and always eager to collaborate in an Agile environment. '+ 
           'I\'m looking for opportunities to apply my skills in real-world projects and make a meaningful impact.'
           ,
 };
